39f39559be7c77454e6c3a122586d2dcc4a64385
msuad <1445617868@qq.com>
2024-06-20 12:05:22 +0800

    好像能玩了

--- Makefile
+++ Makefile
@@ -5,9 +5,20 @@ QEMU_X86_64=${QEMU_BUILD_DIR}/qemu-system-x86_64
 all build:${QEMU_X86_64}
 
 run:build
-	${QEMU_X86_64} -h
+	${QEMU_X86_64} \
+	-machine q35,smm=on,accel=tcg \
+  -m 4G \
+  -smp 4 \
+  -vga virtio \
+  -global driver=cfi.pflash01,property=secure,value=on \
+  -drive if=pflash,format=raw,unit=0,file=OVMF_CODE.fd,readonly=on \
+  -drive if=pflash,format=raw,unit=1,file=OVMF_VARS.fd \
+  -cdrom /Users/asuad/Downloads/ubuntu-24.04-desktop-amd64.iso  #\       # 添加ISO镜像作为光驱设备
+	#-drive file=/path/to/harddisk.raw,if=sata,index=0,media=disk   # 添加SATA硬盘
 
 include build_qemu.mk
 
 dist-clean:
 	- rm -rf ./qemu
+
+#include get_uefi.mk
--- get_uefi.mk
+++ get_uefi.mk
@@ -1,5 +1,29 @@
-preboot_win:
+git clone https://gitcode.com/tianocore/edk2.git --depth=1
+cd OvmfPkg
+
+
+
+all: |preboot_win
+all: |preboot_linux
+preboot_win.zip:preboot_win-clean
 	curl -o preboot_win.zip https://downloadmirror.intel.com/822666/PREBOOT.zip
+preboot_win:preboot_win.zip
 	unzip -o preboot_win.zip -d preboot_win 
 
-preboot_linux:
+
+preboot_linux.tar.gz:preboot_linux-clean
+	curl -o preboot_linux.tar.gz https://downloadmirror.intel.com/822666/Preboot.tar.gz
+preboot_linux: preboot_linux.tar.gz
+	mkdir -p preboot_linux && tar -xvf ./preboot_linux.tar.gz -C preboot_win
+
+
+preboot_clean:preboot_win-clean preboot_linux-clean
+preboot_dist-clean:preboot_clean
+	- rm -rf preboot_win preboot_linux
+
+preboot_win-clean:
+	- rm -f preboot_win.zip ;
+preboot_linux-clean:
+	- rm -f preboot_linux.tar.gz ;
+
+dist-clean:preboot_dist-clean
